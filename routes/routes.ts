3cc330e106aac29cd8bc5c360df961df71007cdd
KcirePunk <kcirepunk22misr@gmail.com>
2020-01-29 18:43:05 -0500

    Primer commit

---
+++ routes/routes.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response } from 'express';
+
+export const router: Router = Router();
+
+router.get('/mensajes', (req: Request, res: Response) => {
+    res.json({
+        ok: true,
+        mensaje: 'Todo esta bien'
+    });
+});
+
+router.post('/mensajes/:id', (req: Request, res: Response) => {
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    const id = req.params.id;
+
+    res.json({
+        ok: true,
+        cuerpo,
+        de,
+        id
+    });
+});
